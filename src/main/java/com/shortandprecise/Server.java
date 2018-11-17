3d9558dca8e540ee9c3122f9c3dedd47f9d3412b
rejuan <rejuan.shuvro@gmail.com>
2018-11-17 17:01:44 +0600

    issue#3 done

--- src/main/java/com/shortandprecise/Server.java
+++ src/main/java/com/shortandprecise/Server.java
@@ -10,8 +10,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -20,6 +18,10 @@ import java.util.logging.Logger;
 public class Server implements Runnable {
 
     private final int port;
+    private static final int FIRST_LINE_POSITION = 0;
+    private static final int METHOD_POSITION = 0;
+    private static final int URI_POSITION = 1;
+    private static final int HTTP_VERSION_POSITION = 2;
     
     public Server(int port) {
         this.port = port;
@@ -35,9 +37,36 @@ public class Server implements Runnable {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
+                        InputStream inputStream;
+                        OutputStream outputStream;
+                        
                         try {
-                            InputStream inputStream = socket.getInputStream();
-                            OutputStream outputStream = socket.getOutputStream();
+                            inputStream = socket.getInputStream();
+                            byte[] buffer = new byte[1024];
+                            StringBuilder stringBuilder = new StringBuilder();
+                            while (true) {                                
+                                int len = inputStream.read(buffer);
+                                if(len > 0) {
+                                    stringBuilder
+                                            .append(new String(buffer, 0, len));
+                                }
+                                
+                                int position = stringBuilder.indexOf("\r\n\r\n");
+                                if(position > 0) {
+                                    String[] request = stringBuilder
+                                            .substring(0, position)
+                                            .split("\r\n");
+                                    
+                                    // TODO need to check request length
+                                    String firstLine = request[FIRST_LINE_POSITION];
+                                    String[] firstLineArray = firstLine.split(" ");
+                                    
+                                    // TODO need to check firstline validity
+                                    String uri = firstLineArray[URI_POSITION];
+                                    System.out.println(uri);
+                                }
+                                
+                            }
                         } catch (IOException ex) {
                             
                         }
